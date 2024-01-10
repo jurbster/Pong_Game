db2e16776a32d5bdb463a361caa7c6ef0e194a81
jurbster <jurbster15@yahoo.com>
2024-01-10 13:50:54 -0600

    score updates

--- pong.py
+++ pong.py
@@ -8,11 +8,15 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Score
+scoreA = 0
+scoreB = 0
+
 # Paddle Left
 paddle_left = turtle.Turtle()
 paddle_left.speed(0)
 paddle_left.shape("square")
-paddle_left.color("white")
+paddle_left.color("teal")
 paddle_left.shapesize(stretch_wid=5, stretch_len=1)
 paddle_left.penup()
 paddle_left.goto(-350, 0)
@@ -21,7 +25,7 @@ paddle_left.goto(-350, 0)
 paddle_right = turtle.Turtle()
 paddle_right.speed(0)
 paddle_right.shape("square")
-paddle_right.color("white")
+paddle_right.color("teal")
 paddle_right.shapesize(stretch_wid=5, stretch_len=1)
 paddle_right.penup()
 paddle_right.goto(350, 0)
@@ -35,6 +39,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.3
 ball.dy = -0.3
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup() #makes it to where you won't see a line when it moves from center
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Move paddles
 def paddle_left_up():
@@ -85,10 +98,18 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        scoreA += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(scoreA, scoreB), align="center", font=("Courier", 24, "normal"))
+
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        scoreB += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(scoreA, scoreB), align="center", font=("Courier", 24, "normal"))
+
 
 # Ball and paddle collide
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_right.ycor() + 50 and ball.ycor() > paddle_right.ycor() - 50):
